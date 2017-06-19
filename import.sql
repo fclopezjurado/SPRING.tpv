9210a2bca108a279ce7d16569cae7d5e1f77106c
Enrique Arranz <kikevillab93@gmail.com>
2017-06-19 18:48:59 +0200

    add import

---
+++ import.sql
@@ -0,0 +1,1 @@
+INSERT INTO `User` (`active`,  `email`, `mobile`, `password`,  `username`) VALUES (b'0', 'admin@gmail.com', 123456789, '$2a$10$V78h.x0EpGctkxXcPxgQyOzJ2ezAR9ltxjm1ReUFbIXbOcDTd7t0e', 'admin');
