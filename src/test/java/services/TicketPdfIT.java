a88d737a16ce21b1cfe7db5dee9db1cc32d1a338
jbernal <jbernal@etsisi.upm.es>
2017-07-13 12:38:39 +0200

    delete error

--- src/test/java/services/TicketPdfIT.java
+++ src/test/java/services/TicketPdfIT.java
@@ -1,27 +1,22 @@
 package services;
 
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import config.PersistenceConfig;
 import config.TestsPersistenceConfig;
-import daos.core.TicketDao;
-import utils.pdfs.PdfGenerator;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {PersistenceConfig.class, TestsPersistenceConfig.class})
 public class TicketPdfIT {
 
-    @Autowired
-    private TicketDao ticketDao;
-
-    //@Test
+    @Test
     //TODO crear un ticket
     public void testGenerate() {
-        new PdfGenerator().generate(ticketDao.findAll().get(0));
+        //new PdfGenerator().generate(ticketDao.findAll().get(0));
     }
 
 }
